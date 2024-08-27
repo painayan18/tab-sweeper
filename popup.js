c5b06a4145345ac0f1213fefdf0697089b65d6e9
nayanpai <nayanspai@gmail.com>
2024-08-27 10:50:31 -0700

    Added tab group names based on domain name.

--- popup.js
+++ popup.js
@@ -1,22 +1,37 @@
+// wait for DOM to fully load
 document.addEventListener('DOMContentLoaded', () => {
+    // listener for sweepButton
     document.getElementById('sweepButton').addEventListener('click', () => {
-        let chrome;
         chrome.tabs.query({}, function (tabs) {
             let groups = {};
 
             tabs.forEach((tab) => {
                 let url = new URL(tab.url);
+
+                // extract hostname from URL
                 let domain = url.hostname;
 
-                if (!groups[domain]) {
-                    groups[domain] = [];
+                // extract domain name from URL
+                // let domainName = domain.replace('www','').split('.')[0];
+
+                let domainName = domain.startsWith('www') ? domain.substring(4) : domain;
+                domainName = domainName.split('.')[0];
+
+                // add domain to groups if it doesn't exist
+                if (!groups[domainName]) {
+                    groups[domainName] = [];
                 }
 
-                groups[domain].push(tab.id);
+                groups[domainName].push(tab.id);
             });
 
-            Object.keys(groups).forEach((domain) => {
-                chrome.tabs.group({ tabIds: groups[domain] });
+            Object.keys(groups).forEach((domainName) => {
+                chrome.tabs.group({ tabIds: groups[domainName] }, function(groupId) {
+                    if (chrome.tabGroups && chrome.tabGroups.update) {
+                        // set group title to domain name
+                        chrome.tabGroups.update(groupId, {title: domainName});
+                    }
+                });
             });
         });
     });
